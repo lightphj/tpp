68b0cfe269e5cc9445dad6587b3f5d3c365f463a
joe <loki961@gmail.com>
2019-10-01 18:57:45 +0900

    Revert "kakao map test"
    This reverts commit 579fdd39

--- joeApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JoeappConfig(AppConfig):
-    name = 'joeApp'
--- joeApp/test.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def mapCrawler(request):
-    return HttpResponse("Django로 Crawler를 만들어 봅시다.")
