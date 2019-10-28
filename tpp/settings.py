727c81b148623cb16e9aee14fb7a8419868be9d8
light <lightphj@naver.com>
2019-10-28 16:25:03 +0900

    views.py

--- tpp/settings.py
+++ tpp/settings.py
@@ -131,3 +131,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tppbot', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
