67994fb7bf756a68a38a16ecdee3c900f5dbd04a
light <lightphj@naver.com>
2019-11-11 18:34:37 +0900

    views.py`

--- hIncruit/models.py
+++ hIncruit/models.py
@@ -3,17 +3,14 @@ from django.utils import timezone
 
 
 # Create your models here.
-class ITEM(models.Model):
-    upper_id = models.CharField(max_length=6, blank=True, null=True, verbose_name='상위ID')
-    group = models.CharField(max_length=10, blank=True, null=True, verbose_name='그룹 ex) 공고,인재상,회사소개')
-    category = models.CharField(max_length=10, blank=True, null=True, verbose_name='카테고리 ex) 공고부문, 주가,뉴스')
+class ANNOUNCEMENT(models.Model):
+    category = models.CharField(max_length=10, blank=True, null=True, verbose_name='직군 코드')
     title = models.CharField(max_length=200, blank=True, null=True, verbose_name='제목')
     content = models.CharField(max_length=1000, blank=True, null=True, verbose_name='내용')
     sub_content = models.TextField(null=True,verbose_name ='상세내용')
     content_img = models.ImageField(blank=True,verbose_name='내용 삽입 이미지')
     thumbnail = models.ImageField(blank=True,verbose_name='썸네일')
-    url = models.CharField(max_length=1000, blank=True, null=True, verbose_name='내용')
-    param = models.CharField(max_length=10, blank=True, null=True, verbose_name='parameter')
+    url = models.CharField(max_length=1000, blank=True, null=True, verbose_name='채용공고 연결 url')
     create_date = models.DateTimeField(default=timezone.now)
     expire_date = models.DateTimeField(null=True)
     last_modify_date = models.DateTimeField(default=timezone.now)
@@ -22,9 +19,31 @@ class ITEM(models.Model):
     def __str__(self):
         return self.title
 
+
+class ANNOUNCE_DETAIL(models.Model):
+    job = models.CharField(max_length=10, blank=True, null=True, verbose_name='직무코드')
+    title = models.CharField(max_length=200, blank=True, null=True, verbose_name='제목')
+    content = models.CharField(max_length=1000, blank=True, null=True, verbose_name='내용')
+    sub_content = models.TextField(null=True, verbose_name='상세내용')
+    content_img = models.ImageField(blank=True, verbose_name='내용 삽입 이미지')
+    thumbnail = models.ImageField(blank=True, verbose_name='썸네일')
+    url = models.CharField(max_length=1000, blank=True, null=True, verbose_name='채용공고 연결 url')
+    rec_person_num = models.CharField(max_length=4, blank=True, null=True, verbose_name='parameter')
+    from_date = models.DateTimeField(null=True,verbose_name='모집기간 from')
+    to_date = models.DateTimeField(null=True,verbose_name='모집기간 to')
+    howto = models.CharField(max_length=1000, blank=True, null=True, verbose_name='지원방법')
+    create_date = models.DateTimeField(default=timezone.now)
+    expire_date = models.DateTimeField(null=True)
+    last_modify_date = models.DateTimeField(default=timezone.now)
+    del_yn = models.CharField(max_length=1, default='N')
+
+    def __str__(self):
+        return self.title
+
+
 class USER(models.Model):
     user_id = models.CharField(max_length=20,default='00000000', verbose_name='kakao chat receive id')
-    regist_category = models.CharField(max_length=10, blank=True, null=True, verbose_name='즐겨찾기 등록한 부문, 비었으면 전체')
+    regist_category = models.CharField(max_length=10, blank=True, null=True, verbose_name='직군 즐겨찾기')
     send_notice_yn = models.CharField(max_length=1, default='N')
     create_date = models.DateTimeField(default=timezone.now)
     expire_date = models.DateTimeField(null=True)
@@ -37,10 +56,8 @@ class USER(models.Model):
 class QUESTION(models.Model):
     poll_id = models.IntegerField(max_length=4, default=-1, verbose_name='설문 ID')
     question_id = models.IntegerField(max_length=4, default=-1, verbose_name='문항 ID')
-    subject = models.CharField(max_length=200, default='subject', verbose_name='제목')
-    content = models.CharField(max_length=1000, default='content', verbose_name='내용')
-    order = models.IntegerField(max_length=4,null=True,blank=True,db_index=True, verbose_name='순서')
-    type = models.CharField(max_length=6, blank=True, null=True, verbose_name='답변형식')
+    subject = models.CharField(max_length=200, default='subject', verbose_name='질문')
+    order_no = models.IntegerField(max_length=4,null=True,blank=True,db_index=True, verbose_name='문항번호')
     create_date = models.DateTimeField(default=timezone.now)
     expire_date = models.DateTimeField(null=True)
     last_modify_date = models.DateTimeField(default=timezone.now)
@@ -54,3 +71,19 @@ class ANSWER(models.Model):
     question_id = models.IntegerField(max_length=4, default=-1, verbose_name='문항 ID')
     user_id = models.ForeignKey(USER, on_delete=models.DO_NOTHING())
     val = models.CharField(max_length=10, default='0', verbose_name='답변값')
+
+class QUESTION_CATEGORY(models.Model):
+    poll_id = models.IntegerField(max_length=4, default=-1, verbose_name='설문 ID')
+    question_id = models.IntegerField(max_length=4, default=-1, verbose_name='문항 ID')
+    category = models.CharField(max_length=4, default=' ', verbose_name='직군')
+    answer_yn = models.CharField(max_length=1, default=' ', verbose_name='Y/N 일때 +1')
+
+
+class CATEGORY(models.Model):
+    category_cd = models.CharField(max_length=4, default=' ', verbose_name='직군코드')
+    category_nm = models.CharField(max_length=4, default=' ', verbose_name='직군명')
+    explain = models.TextField(null=True,blank=True, verbose_name='직군 상세설명')
+    create_date = models.DateTimeField(default=timezone.now)
+    expire_date = models.DateTimeField(null=True)
+    last_modify_date = models.DateTimeField(default=timezone.now)
+    del_yn = models.CharField(max_length=1, default='N')
--- hIncruit/views.py
+++ hIncruit/views.py
@@ -1,3 +1,102 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from hIncruit.models import *
+from django.utils import timezone
+import json
+import logging
+logger = logging.getLogger(__name__)
 # Create your views here.
+
+@csrf_exempt
+def startQuestion(request):
+
+
+    todo = '''
+    1. 데이터를 받는다 ( user: user_id, clientExtra : 설문ID,문항번호,답변YN)
+    2. 받은 데이터를 DB저장
+    3. 설문ID와 문항번호를 이용해서 다음 설문데이터를 꺼낸다
+    3-1. 마지막 설문이었다면, 챗봇에 결과보기라는 버튼을 주고 눌렀을때 설문결과페이지로 가야한다.
+    4. 챗봇에 json 으로 설문전달
+    5. 각 설문의 답변 버튼에 extra 로 설문id,문항번호,답변YN 값이 들어가있어야하고
+    6. 각 답변은 모두 클릭시 현재 블럭ID로 재연결 해야한다.
+    
+    '''
+    json_str = ((request.body).decode('utf-8'))
+    received_json_data = json.loads(json_str)
+    botUserKey = ''
+    try:
+        botUserKey = received_json_data['userRequest']['user']['id']
+    except TypeError:
+        logger.error("json data parising error")
+    except ValueError:
+        logger.error("json data parising error")
+    except NameError:
+        logger.error("json data parising error")
+    except SyntaxError:
+        logger.error("json data parising error")
+    logger.info(type(received_json_data))
+    print(json.dumps(received_json_data, indent=4, sort_keys=True))
+    #tfp = place.objects.filter(category_group_code=category).order_by('distance')[:5]
+    isFirst = 'y'
+    jsonstr = ''
+    logger.info(botUserKey)
+
+    fulljson = '''{
+  "version": "1.0",
+  "template": {
+    "outputs": [
+      {
+        "basicCard": {
+          "title": "보물상자",
+          "description": "보물상자 안에는 뭐가 있을까",
+          "thumbnail": {
+            "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
+          },
+          "profile": {
+            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
+            "nickname": "보물상자"
+          },
+          "social": {
+            "like": 1238,
+            "comment": 8,
+            "share": 780
+          },
+          "buttons": [
+            {
+              "action": "message",
+              "label": "열어보기",
+              "messageText": "짜잔! 우리가 찾던 보물입니다"
+            },
+            {
+              "action":  "webLink",
+              "label": "구경하기",
+              "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+            }
+          ]
+        }
+      }
+    ]
+  }
+}'''
+
+    #logger.info("\n\n fulljson : \n" + fulljson)
+    # print(fulljson)
+
+    dictjson = json.loads(fulljson)
+
+    # print(dictjson)
+    # places = place.objects.filter(category_group_code='01')
+    return JsonResponse(dictjson
+                        )
+
+
+def endQuestion(request):
+    todo = '''
+    1. 유저 데이터를 받는다 ( user: user_id)
+    2. userid 로 가장 최근의 설문조사 결과를 토대로 맞는 직군을 선정한다. (아니면 clientExtra 로 설문번호도 받아올까?)
+    2-1. 섬룬조사 결과를 iter 하면서 해당 문항과 YN 의 답변이 일치하는 question_category 를 찾아서 +1 한다 
+    2-1-1. 그러려면 dict 로 먼저 전체직군,값(초기값 0) 을 가져와야한다.
+    2-1. iter를 다돌면, 가장 높은 점수를 찾고, 동일점수가 여러개일시 '~~~~' 의방법으로 order 해서 한줄만 가져온다.
+    3. 가져온 직무와 그 내용을 뿌려주고, 다시 설문하기와 처음으로 돌아가기 quickRepl 을 준다.
+    '''
