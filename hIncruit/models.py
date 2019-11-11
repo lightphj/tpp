60f31d93fa72d648583f572ae969470a918840ef
joe <loki961@gmail.com>
2019-11-11 19:22:14 +0900

    correct error

--- hIncruit/models.py
+++ hIncruit/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class ANNOUNCEMENT(models.Model):
     category = models.CharField(max_length=10, blank=True, null=True, verbose_name='직군 코드')
-    screening = models.CharField(max_length=10 blank=True, null=True, verbose_name='채용 구분')
+    screening = models.CharField(max_length=10, blank=True, null=True, verbose_name='채용 구분')
     title = models.CharField(max_length=200, blank=True, null=True, verbose_name='제목')
     content = models.CharField(max_length=1000, blank=True, null=True, verbose_name='내용')
     sub_content = models.TextField(null=True,verbose_name ='상세내용')
