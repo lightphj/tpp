ae4227b9f215fdc332178c924134fd19b7083f74
joe <loki961@gmail.com>
2019-12-06 10:47:35 +0900

    add

--- hIncruit/stocks.py
+++ hIncruit/stocks.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def get_url(item_name, code_df): 
     code = code_df.query("name=='{}'".format(item_name))['code'].to_string(index=False) 
